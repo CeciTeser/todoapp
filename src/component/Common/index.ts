58187d03ad6784237edc07d9eaa9ed04290a2de6
Austyn <austyn@MacBook-Air-de-Austyn.local>
2021-12-23 10:57:28 +0100

    fix/bug loading component duplicate file delete

--- src/component/Common/index.ts
+++ src/component/Common/index.ts
@@ -1,6 +1,6 @@
-export {Loading} from './Loading '
-export {Footer} from './Footer'
-export {Header} from './Header'
-export {Logo} from './Logo'
-export {Nav} from './Nav'
-export {Contact} from './Contact'
+export {Loading} from './Loading';
+export {Footer} from './Footer';
+export {Header} from './Header';
+export {Logo} from './Logo';
+export {Nav} from './Nav';
+export {Contact} from './Contact';
